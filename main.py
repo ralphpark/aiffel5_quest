74d9680309799855db2f33352c0923528a7f18cd
ralphpark <ralphpark@knou.ac.kr>
2023-06-28 14:21:12 +0900

    add node4

--- main.py
+++ main.py
@@ -4,14 +4,14 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def hi():
+    print('hi')
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print("hello")
+def deco(fn):
+    def wrapper():
+        print('*'*20)
+        fn()
+        print('*'*20)
+    return wrapper
+deco_hey = deco(hi)
+deco_hey()
