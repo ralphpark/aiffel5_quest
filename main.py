8e4f78f8117265c6dcde16c1951117e4bb1c2d50
ralphpark <ralphpark@knou.ac.kr>
2023-06-28 16:27:47 +0900

    add node4

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# This is a sample Python script.
+nums = list(range(1, 11))
+result = 0
+for i in range(10):
+    result+= nums.pop()
+print(result)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+dict_uni  = {'a': 97, 'b': 98, 'c': 99, 'd': 100, 'e': 101, 'f': 102}
+dict_uni.popitem()
+print(dict_uni)
 
 
-def hi():
-    print('hi')
 
-def deco(fn):
-    def wrapper():
-        print('*'*20)
-        fn()
-        print('*'*20)
-    return wrapper
-deco_hey = deco(hi)
-deco_hey()
+
+parks_age = [12, 18, 21, 22, 25, 27, 30, 32, 35]
+parks_dic = dict.fromkeys(parks_age, 'age')
+print(parks_dic)
