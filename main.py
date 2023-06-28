def4998cd418b23faa931e91af1278b597fb5b98
ralphpark <ralphpark@knou.ac.kr>
2023-06-28 18:19:12 +0900

    add node4

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-nums = list(range(1, 11))
-result = 0
-for i in range(10):
-    result+= nums.pop()
-print(result)
+
+def deco(fn) :
+    def wrapper():
+        print('*'*20)
+        fn()
+        print('*'*20)
+    return wrapper
+
+@deco
+def hi():
+    print("hi")
 
 
-dict_uni  = {'a': 97, 'b': 98, 'c': 99, 'd': 100, 'e': 101, 'f': 102}
-dict_uni.popitem()
-print(dict_uni)
-
-
-
-
-parks_age = [12, 18, 21, 22, 25, 27, 30, 32, 35]
-parks_dic = dict.fromkeys(parks_age, 'age')
-print(parks_dic)
+print(hi())
