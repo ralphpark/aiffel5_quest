63a089042756973b415443892d72c7416af38caf
ralphpark <ralphpark@knou.ac.kr>
2023-06-30 17:48:55 +0900

    add node5

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-# 실습퀴즈3. 일급 객체
-# reduce 함수를 함께 사용하여 함수를 매개변수로 전달하는 코드를 작성해 보세요.
+# 실습퀴즈4. 문자열의 길이를 판별하는 함수를 만들어 보세요.
 
-from functools import reduce
+def closure_str_checker():
+    # 문자열이 길면 알려주는 함수
+    def check_length(limit):
+        def length_func(string):
+            if len(string) > limit:
+                return f'길이는 {len(string)}개 입니다. 지정한 길이보다 길어요.' 
+            else:
+                return "길이가 적당합니다."
+        return length_func
+
+    def str_checker(string):
+        num_len = 20 # 길이를 설정합니다. 
+        length = check_length(num_len)
+        if len(string) <= 0:
+            print('문자열의 길이가 0보다 작거나 같습니다.')
+        else:
+            print('문자열의 길이 :', len(string), length(string), sep='\n')
+    return str_checker
+
+check = closure_str_checker()
+test_str = '화이팅. 아자 아자 화이팅!!!!!!!!' # 길이가 다른 문자열을 입력해보세요.
+check(test_str)
