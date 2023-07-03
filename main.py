d736c33a637b482f418f3fef7fb61d84685a8a0f
ralphpark <ralphpark@knou.ac.kr>
2023-07-03 14:21:39 +0900

    add node7

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
+# 복습퀴즈. closure를 직접 구현해봅시다. # [[YOUR CODE]]에 들어가야할 코드는 무엇일까요? 
 
+def int_plus(n):
+    def plus(x):
+        return n + x
+    return plus
 
-# 리듀스는 팩토리알과 같은 계산방법이다!
-from functools import reduce
-def f(x,y):
-    return x*y
-a = [1,2,3,4,5]
-aa = reduce(f,a)
-print(aa)
-
-aaa = reduce(lambda x,y:x*y, a)
-print(aaa)
-# Q. 리스트 컴프리헨션과 람다 표현식을 이용해 1부터 100까지의 합을 구하는 코드를 구현해보세요!
-aaaa = reduce(lambda x,y:x+y,([i for i in range(0,101)]))
-print(aaaa)
-
+# 결과 확인
+closure_plus = int_plus(2)
+print(closure_plus(3))
