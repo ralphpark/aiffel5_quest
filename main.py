f9a83811148dd8b7329526c4b936703a8dd790a8
ralphpark <ralphpark@knou.ac.kr>
2023-07-05 14:58:52 +0900

    add node9

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import calc
+# [Playground] 좋아하는 영화의 주인공 이름을 넣어서 Aiffel 클래스의 객체를 만들어보세요!
+class Aiffel :
+    def __init__(self,actor):
+        self.actor = actor
+    def movie_actor(self):
+        print(f"내가 좋아하는 영화 주인공은 {self.actor} 입니다.")
 
-print(calc.power(10, 5))
-print(calc.cubic(0.1))
-print(calc.divide(10, 7))
-print(calc.factorial(10)) # !10 = 10*9*8*7*6*5*4*3*2*1
+like_movie = Aiffel('로버트 다우니 주니어')
+like_movie.movie_actor()
+
