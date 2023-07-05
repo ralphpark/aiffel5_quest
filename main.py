2dd21c11cc90c9687ae4936f5381d12388ebb5f0
ralphpark <ralphpark@knou.ac.kr>
2023-07-05 18:23:19 +0900

    add node10

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-# [Playground] 좋아하는 영화의 주인공 이름을 넣어서 Aiffel 클래스의 객체를 만들어보세요!
-class Aiffel :
-    def __init__(self,actor):
-        self.actor = actor
-    def movie_actor(self):
-        print(f"내가 좋아하는 영화 주인공은 {self.actor} 입니다.")
+# 문제 2-1. Kiosk 클래스를 생성합니다.
+# 전체 메뉴와 가격
+menu = ['americano', 'latte', 'mocha', 'yuza_tea', 'green_tea', 'choco_latte']
+price = [2000, 3000, 3000, 2500, 2500, 3000]
+class Kiosk:
+    def __init__(self):
+        self.menu = menu
+        self.price = price
 
-like_movie = Aiffel('로버트 다우니 주니어')
-like_movie.movie_actor()
+    def menu_print(self):
+        for i in range(len(self.menu)):
+            print(i + 1, self.menu[i], ' : ', self.price[i], '원')
 
+    # 문제 2-2. 주문 메서드
+    # 음료의 이름과 가격을 출력하세요.
+
+#     def menu_select(self):
+#         n = int(input("음료의 번호를 입력하세요 : "))  # 음료 번호 입력
+#         self.price_sum = self.price[n - 1]  # 선택 음료의 가격
+#         # 문제 2-2. 음료의 이름과 가격을 출력하세요.
+#         print(  , ' : ', , '원')  # 메뉴
+#
+# a = Kiosk()
+# print(a.menu_print())
