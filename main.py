c6f6abe14efbd873a7ab38307f8424faa952a369
ralphpark <ralphpark@knou.ac.kr>
2023-07-06 11:52:14 +0900

    add node10

--- main.py
+++ main.py
@@ -48,13 +48,15 @@ class Kiosk:
             if n > 0 and n < len(self.menu) + 1:
                 # 추가 음료 온도
                 # 문제 2-4. 추가 음료의 온도를 입력받고, 가격 리스트, 주문 리스트, 합계 금액을 업데이트해보세요.
-                t = int(input("HOT 음료는 1을, ICE 음료는 2를 입력하세요 : "))
-                if t == 1:
-                    temp = "HOT"
-                elif t == 2:
-                    temp = "ICE"
-                else:
-                    print("1과 2 중 하나를 입력하세요.\n")
+                t=0
+                while t != 1 and t != 2:
+                    t = int(input("HOT 음료는 1을, ICE 음료는 2를 입력하세요 : "))
+                    if t == 1:
+                        temp = "HOT"
+                    elif t == 2:
+                        temp = "ICE"
+                    else:
+                        print("1과 2 중 하나를 입력하세요.\n")
                 self.order_menu.append(temp + ' ' + self.menu[n - 1])
                 self.order_price.append(self.price[n - 1])
                 print('추가 주문 음료',  temp, self.menu[n-1], ':', self.price[n-1], '원\n', '합계 : ',  sum(self.order_price) , '원')
@@ -67,16 +69,18 @@ class Kiosk:
     def pay(self):
         if sum(self.order_price) != 0:
             print("총 금액은", sum(self.order_price), "원입니다.")
-            money = int(input("지불수단을 선택해주세요. 현금은 1번, 카드는 2번 : "))
-            if money == 1:
-                print("직원을 호출하겠습니다..")
-            elif money == 2:
-                print("IC칩 방향에 맞게 카드를 꽂아주세요")
-            else:
-                print("다시 결제를 시도해 주세요.")
-
+            #지불 방법
+            self.pay_method = 0
+            while self.pay_method != 1 and self.pay_method != 2:
+                self.pay_method = int(input("지불수단을 선택해주세요. 현금은 1번, 카드는 2번 : "))
+                if self.pay_method == 1:
+                    print("직원을 호출하겠습니다.")
+                elif self.pay_method == 2:
+                    print("IC칩 방향에 맞게 카드를 꽂아주세요")
+                else:
+                    print("다시 결제를 시도해 주세요.")
         else:
-            print("주문 내역이 없습니다.")
+            print("주문이 완료되지 않았습니다.")
     def table(self):
         print('⟝' + '-' * 30 + '⟞')
         for i in range(5):
