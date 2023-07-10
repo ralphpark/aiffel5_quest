2ed756a7dffc4ea96f5a3c05d8fa479a9e1efbd8
ralphpark <ralphpark@knou.ac.kr>
2023-07-10 18:22:37 +0900

    module3_node2 update

--- Module03/test.py
+++ Module03/test.py
@@ -5,7 +5,6 @@
 import pandas as pd
 human_list = ['엄마', '아버지', '와이프']
 thanks_list = ['항상 고맙고 사랑해', '고맙고 사랑해', '고맙고 사랑해']
-df = pd.D
-ataFrame({'human': human_list, 'thanks': thanks_list})
+df = pd.DataFrame({'human': human_list, 'thanks': thanks_list})
 df.to_csv('thanks.csv', index=False)
 print(df)
