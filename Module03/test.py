cf1fe263cc876b4dcf91e4689abfeb38fed0bb97
ralphpark <ralphpark@knou.ac.kr>
2023-07-13 20:22:13 +0900

    module3_node6 update

--- Module03/test.py
+++ Module03/test.py
@@ -1,34 +1,29 @@
-# 데이터 로드
-def make_dataset2():
-    dataset = load_diabetes()
-    df = pd.DataFrame(dataset.data, columns=dataset.feature_names)
-    df['target'] = dataset.target
-    return df.drop('target', axis=1), df['target']
-X, y = make_dataset2()
-# Q. 위 데이터로 엘라스틱넷 회귀를 돌려볼 예정입니다.
-# RandomizedSearchCV 를 활용하여 다음 두 하이퍼파라미터 중 최적을 찾고,
-# 'alpha':[0.0001, 0.001, 0.01, 0.1, 1]
-# 'l1_ratio':[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-# 최적의 하이퍼파라미터로 모델을 돌려 rmse를 측정해주세요.
+import  pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-params={# [[YOUR CODE]]}
+# 데이터 불러오기
+from sklearn.datasets import load_breast_cancer
+def make_dataset():
+    iris = load_breast_cancer()
+    df = pd.DataFrame(iris.data, columns = iris.feature_names)
+    df['target'] = iris.target
+    X_train, X_test, y_train, y_test = train_test_split(df.drop('target', axis = 1), df['target'], test_size = 0.5, random_state = 1004)
+    return X_train, X_test, y_train, y_test
+X_train, X_test, y_train, y_test = make_dataset()
 
-from sklearn.linear_model import ElasticNet
-model = ElasticNet()
-model.fit(# [[YOUR CODE]])
-pred = model.predict(# [[YOUR CODE]])
+# 타겟 확인
+print(y_train.value_counts())
 
-grid = RandomizedSearchCV(# [[YOUR CODE]])
-grid.fit(# [[YOUR CODE]])
+# 모델 생성
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+model = DecisionTreeClassifier(criterion='entropy',
+                               max_depth=3,
+                               min_samples_split=2,
+                               min_samples_leaf=2,
+                               random_state=0)
+model.fit(X_train, y_train)
+pred = model.predict(X_test)
+print(accuracy_score(y_test, pred))
 
-# 최적의 하이퍼파라미터 찾기
-grid.best_params_
-
-
-# 찾은 최적의 하이퍼파라미터로 모델을 돌려 rmse를 측정하세요.
-model = ElasticNet(# [[YOUR CODE]])
-model.fit(# [[YOUR CODE]])
-pred = model.predict(# [[YOUR CODE]])
-
-import numpy as np
-# [[YOUR CODE]]
