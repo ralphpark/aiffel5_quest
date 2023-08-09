1f99215b860653c0a8d8293d41c3e8684d062d91
ralphpark <ralphpark@knou.ac.kr>
2023-08-09 11:21:00 +0900

    Main Module05 update

--- Module05/test.py
+++ Module05/test.py
@@ -1,21 +1,110 @@
-# (100, 100, 3) 형태의 데이터를 받는 Input 레이어를 쌓으세요
+from keras.datasets import reuters
+import numpy as np
+
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
+word_index = reuters.get_word_index()
+index_word = dict([(value, key) for (key, value) in word_index.items()])
+news = ' '.join([str(i) for i in train_data[0]])
+news = ' '.join([index_word.get(i-3, '?') for i in train_data[0]])
+
+def one_hot_encoding(data, dim=10000):
+  results = np.zeros((len(data), dim))
+  for i, d in enumerate(data):
+    results[i, d] = 1.
+  return results
+
+x_train = one_hot_encoding(train_data)
+x_test = one_hot_encoding(test_data)
+
+from tensorflow.keras import utils
+
+y_train = utils.to_categorical(train_labels)
+y_test = utils.to_categorical(test_labels)
+
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from tensorflow.keras import models, layers
 
+model = models.Sequential()
+model.add(layers.Dense(128, activation='relu', input_shape=(10000, ), name='input'))
+model.add(layers.Dense(128, activation='relu', name='hidden'))
+model.add(layers.Dense(46, activation='softmax', name='output'))
 
-inputs = layers.Input(shape=(100, 100, 3))
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
-# Flatten 레이어를 쌓으세요.
-x = layers.Flatten()(inputs)
+history = model.fit(x_train, y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_test, y_test))
 
-# Unit의 수는 400, 활성화함수는 ReLU를 사용하는 Dense 레이어를 쌓으세요.
-x = layers.Dense(400, activation='relu')(x)
+def build_model(initializer):
+  model = models.Sequential()
+  model.add(layers.Dense(128,
+                         activation='relu',
+                         kernel_initializer=initializer,
+                         input_shape=(10000, ),
+                         name='input'))
+  model.add(layers.Dense(128,
+                         activation='relu',
+                         kernel_initializer=initializer,
+                         name='hidden'))
+  model.add(layers.Dense(46,
+                         activation='softmax',
+                         name='output'))
+  model.compile(optimizer='rmsprop',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+  history = model.fit(x_train, y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_test, y_test))
+  return history
 
-# Unit의 수는 200, 활성화함수는 ReLU를 사용하는 Dense 레이어를 쌓으세요.
-x = layers.Dense(200, activation='relu')(x)
+def diff_history(history1, history2):
+  history1_dict = history1.history
+  h1_loss = history1_dict['loss']
+  h1_val_loss = history1_dict['val_loss']
 
-# Unit의 수는 100, 활성화함수는 Softmax를 사용하는 Dense 레이어를 쌓으세요.
-x = layers.Dense(100, activation='softmax')(x)
+  history2_dict = history2.history
+  h2_loss = history2_dict['loss']
+  h2_val_loss = history2_dict['val_loss']
 
-model = models.Model(inputs=inputs, outputs=x)
-model.summary()
+  epochs = range(1, len(h1_loss) + 1)
+  fig = plt.figure(figsize=(12, 5))
+  plt.subplots_adjust(wspace=0.3, hspace=0.3)
+
+  ax1 = fig.add_subplot(1, 2, 1)
+  ax1.plot(epochs, h1_loss, 'b-', label='train_loss')
+  ax1.plot(epochs, h1_val_loss, 'r-', label='val_loss')
+  ax1.plot(epochs, h2_loss, 'b--', label='train_loss')
+  ax1.plot(epochs, h2_val_loss, 'r--', label='val_loss')
+  ax1.set_title('Train and Validation Loss')
+  ax1.set_xlabel('Epochs')
+  ax1.set_ylabel('Loss')
+  ax1.grid()
+  ax1.legend()
+
+  h1_accuracy = history1_dict['accuracy']
+  h1_val_accuracy = history1_dict['val_accuracy']
+
+  h2_accuracy = history2_dict['accuracy']
+  h2_val_accuracy = history2_dict['val_accuracy']
+
+  ax2 = fig.add_subplot(1, 2, 2)
+  ax2.plot(epochs, h1_accuracy, 'b-', label='train_accuracy')
+  ax2.plot(epochs, h1_val_accuracy, 'r-', label='val_accuracy')
+  ax2.plot(epochs, h2_accuracy, 'b--', label='train_accuracy')
+  ax2.plot(epochs, h2_val_accuracy, 'r--', label='val_accuracy')
+  ax2.set_title('Train and Validation Accuracy')
+  ax2.set_xlabel('Epochs')
+  ax2.set_ylabel('Accuracy')
+  ax2.grid()
+  ax2.legend()
+
+  plt.show()
+
+from tensorflow.keras import initializers
+
+zero_history = build_model(initializers.Zeros())
+
